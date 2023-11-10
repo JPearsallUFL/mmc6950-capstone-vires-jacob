d5f528d3be3d6b09375b48b3f337f750c5c114ff
Vires <Jacob.Vires@disney.com>
2023-11-10 09:11:37 -0500

    Logout not refreshing headed troubleshoot

--- hooks/useLogout.js
+++ hooks/useLogout.js
@@ -4,10 +4,10 @@ export default function useLogout() {
   const router = useRouter()
   return async function handleLogout() {
     try {
+      router.push("/")
       const res = await fetch('/api/auth/logout', {method: 'POST'})
       if (res.status === 200)
         console.log("User Logged Out")
-        router.push("/")
     } catch(err) {
       console.log(err)
     }
