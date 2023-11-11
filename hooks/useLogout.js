280fc037fcb1445176c2ebf49e792aa0c980a47b
Vires <Jacob.Vires@disney.com>
2023-11-11 13:24:33 -0500

    add router.replace to logout hook

--- hooks/useLogout.js
+++ hooks/useLogout.js
@@ -8,6 +8,7 @@ export default function useLogout() {
       const res = await fetch('/api/auth/logout', {method: 'POST'})
       if (res.status === 200)
         console.log("User Logged Out")
+        router.replace(router.asPath)
     } catch(err) {
       console.log(err)
     }
